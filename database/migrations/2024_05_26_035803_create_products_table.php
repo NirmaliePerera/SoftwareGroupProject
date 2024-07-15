9c9953b114b510f67c943393489ac38ccb3d7cf6
NirmaliePerera <154779829+NirmaliePerera@users.noreply.github.com>
2024-07-15 10:32:00 +0530

    Invoice corrected

--- database/migrations/2024_05_26_035803_create_products_table.php
+++ database/migrations/2024_05_26_035803_create_products_table.php
@@ -17,9 +17,15 @@ return new class extends Migration
             $table->string('category');
             $table->string('color');
             $table->string('size');
+<<<<<<< Updated upstream:database/migrations/2024_05_26_035830_create_products_table.php
             $table->integer('quantity');
             $table->decimal('initial_price');
             $table->decimal('last_rented_price');
+=======
+            $table->integer('quantity')->default(0);
+            $table->decimal('initial_price', 8, 2);
+            $table->decimal('last_rented_price', 8, 2)->default(0.00);
+>>>>>>> Stashed changes:database/migrations/2024_05_26_035803_create_products_table.php
             $table->text('description');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,13 @@ Route::middleware(['admin_or_employee'])->group(function () {
     Route::delete('/{appointment}', [AppointmentController::class, 'destroy'])->name('appointments.destroy');
 
     //invoice routes
-    Route::resource ('invoice',InvoiceController::class);
+    Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoices.index');
+    Route::get('/invoices/create', [InvoiceController::class, 'create'])->name('invoices.create');
+    Route::post('/invoices', [InvoiceController::class, 'store'])->name('invoices.store');
+    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show'])->name('invoices.show');
+    Route::get('/invoices/{invoice}/edit', [InvoiceController::class, 'edit'])->name('invoices.edit');
+    Route::put('/invoices/{invoice}', [InvoiceController::class, 'update'])->name('invoices.update');
+    Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy'])->name('invoices.destroy');
 });
 
 //Customer routes
